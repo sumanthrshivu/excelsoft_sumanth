f67a80655d9c4f71b9b3ae9e262114793e7e1c3f
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-21 21:44:13 +0530

    encapsulation program

---
+++ encapsulation demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace encapsulation_demo
+{
+    class studentInfo
+    {
+
+        private string id;
+        private string name;
+        private int marks;
+
+        public string std_id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string std_name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int std_marks
+        {
+            get { return marks; }
+            set { marks = value; }
+        }
+
+    }
+
+    class stdinfo
+    {
+        static void Main()
+        {
+            studentInfo s1 = new studentInfo();
+            s1.std_id = "12";
+            s1.std_name = "sumanth";
+            s1.std_marks = 80;
+
+            Console.WriteLine(s1.std_id);
+            Console.WriteLine(s1.std_name);
+            Console.WriteLine(s1.std_marks);
+
+        }
+    }
+
+}
