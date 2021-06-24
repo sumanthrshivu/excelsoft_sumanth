4b9effe9a7fb8687b28d3e9437e7723982b4fb7d
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-24 16:45:08 +0530

    bmi calculator program

---
+++ bmicalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bmicalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bmi b = new bmi();
+            Console.Write("Weight in kg: ");
+            b.weight_kg = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height in m: ");
+            b.height_m = Convert.ToDouble(Console.ReadLine());
+
+            b.bmi_calc= b.weight_kg / b.height_m * 2;
+
+            if (b.bmi_calc < 18.5)
+            { Console.WriteLine("-> Underweight"); }
+            if (b.bmi_calc >= 19 & b.bmi_calc <= 24)
+            { Console.WriteLine("-> healthy"); }
+            if (b.bmi_calc >= 25)
+            { Console.WriteLine("-> Overweight"); }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bmicalculator/bmi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bmicalculator
+{
+    class bmi
+    {
+        private int kg;
+        private double m;
+        private double bmical;
+
+        public int weight_kg
+        {
+            set { this.kg = value; }
+            get { return this.kg; }
+        }
+
+        public double height_m
+        {
+            set { this.m = value; }
+            get { return this.m; }
+        }
+
+        public double bmi_calc
+        {
+            set { this.bmical = value; }
+            get { return this.bmical; }
+        }
+
+    }
+
+   
+}
