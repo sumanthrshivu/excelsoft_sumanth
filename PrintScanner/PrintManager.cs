e78ffac94caf9715a28f41f8cb1d621553a90136
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-28 19:33:52 +0530

    PrintScanner Program

---
+++ PrintScanner/PrintManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintScannerProgram 
+{
+
+    public class PrintManager
+    {
+        IPrinter _Printer;
+        public void PrintDocument(Printer printer)
+        {
+            this._Printer = printer;
+        }
+
+    }
+}
---
+++ PrintScanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrintScannerProgram
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Printer _printerRef = new Printer();
+            Scanner _scannerRef = new Scanner();
+
+            PrintManager _printManager = new PrintManager();
+            _printManager.PrintDocument(_printerRef);
+
+            SacnManager _scanManager = new SacnManager();
+            _scanManager.ScanDocument(_scannerRef);
+
+            PrintScanner _printScanner = new PrintScanner();
+            _printManager.PrintDocument(_printScanner);
+            _scanManager.ScanDocument(_printScanner);
+
+
+        }
+    }
+}
