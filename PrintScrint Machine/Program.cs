7b547adcee1cf93ffafdbacdf20a7ca02e4d5c76
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-06-30 16:41:45 +0530

    PrintScanner using double interface

---
+++ PrintScrint Machine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintScanner_Machine
+
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            PrintManager _printDocument = new PrintManager();
+            _printDocument.Print();
+
+            ScanManager _scanDcoument = new ScanManager();
+            _scanDcoument.Scan();
+
+            PrintScanner _printScan = new PrintScanner(_printDocument, _scanDcoument);
+            _printScan.PrintDcoument();
+            _printScan.ScanDcoument();
+        }
+    }
+}
