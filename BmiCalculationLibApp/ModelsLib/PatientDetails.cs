4d7b7d959dff6abbd717c1d6fceadcd841da5234
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-09 10:03:48 +0530

    BmiLipAPP using csvFileReader

---
+++ BmiCalculationLibApp/ModelsLib/PatientDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLib
+{
+    public class PatientDetails
+    {
+        public string Name { get; set; }
+        public double height { get; set; }
+
+        public double weight { get; set; }
+    }
+}
