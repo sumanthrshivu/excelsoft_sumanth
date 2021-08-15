dab77399539a810fd47a9489a907155feeb6ddd4
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-08-15 16:53:58 +0530

    2

---
+++ BmiCalculatorApp/BmiCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BmiCalculatorApp
+{
+    public class BmiCalculator
+    {
+        public double CalculateBmi(double Weight_Kg, double Height_M) //body mass index calculation
+        {
+                return Weight_Kg / Height_M;
+        }
+         
+    }
+}
